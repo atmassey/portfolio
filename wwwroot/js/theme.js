33aeb4644a598aa239244d37d6d1997821c6abbb
Austin Massey <54543946+atmassey@users.noreply.github.com>
2025-12-25 06:30:13 -0600

    added js for theme

---
+++ wwwroot/js/theme.js
@@ -0,0 +1,19 @@
+﻿const getStoredTheme = () => localStorage.getItem('theme')
+const getPreferredTheme = () => {
+    const storedTheme = getStoredTheme()
+    if (storedTheme) return storedTheme
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+}
+
+const setTheme = (theme) => {
+    document.documentElement.setAttribute('data-bs-theme', theme)
+}
+
+setTheme(getPreferredTheme())
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+    const storedTheme = getStoredTheme()
+    if (storedTheme !== 'light' && storedTheme !== 'dark') {
+        setTheme(getPreferredTheme())
+    }
+})
